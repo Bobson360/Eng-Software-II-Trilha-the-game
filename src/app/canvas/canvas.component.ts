9d729e5de2ef7947ce549740f711f698b24f0106
Robson Rodrigues <bobson278@gmail.com>
2022-04-27 22:09:52 -0300

    canvas configurado

---
+++ src/app/canvas/canvas.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.css'],
+  template: `
+    <canvas #canvas width="300" height="300"></canvas>
+    <button (click)="animate()">Play</button>
+  `,
+  styles: ['canvas { border-style: solid }']
+})
+export class CanvasComponent implements OnInit {
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  private ctx: CanvasRenderingContext2D;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+}
